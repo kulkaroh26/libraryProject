349a222f86c580631b3ab48352d6cf0b8d98ac5e
kulkaroh26 <114490265+kulkaroh26@users.noreply.github.com>
2025-01-24 09:36:26 -0500

    Update doubleCrosswalk.java

--- doubleCrosswalk.java
+++ doubleCrosswalk.java
@@ -1,19 +1,21 @@
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
-import java.util.ArrayList;
+import java.util.ArrayList; //Import this to handle ArrayList methods
 
 public class doubleCrosswalk {
   public static void main(String[] args) {
     ArrayList<Integer> barcode = new ArrayList<Integer>();
+    ArrayList<Integer> stuID = new ArrayList<Integer>();
+    //first try catch to handle exceptions 
     try {
-      File myObj = new File("StudentBarcodeIDS.csv");
+      File myObj = new File("StudentBarcodeIDS.csv"); //import file for student barcodes originally
       Scanner myReader = new Scanner(myObj);
       myReader.nextLine();
       while (myReader.hasNextLine()) {
         String data = myReader.nextLine();
         barcode.add(Integer.valueOf(data));
-        System.out.println(data);
+        //System.out.println(data);
       }
       myReader.close();
 
@@ -22,33 +24,36 @@ public class doubleCrosswalk {
       e.printStackTrace();
     }
     try {
-      File myObj = new File("Export Items, Copy-Based-3.txt");
+      File myObj = new File("Export Items, Copy Based-3.txt");
       Scanner myReader = new Scanner(myObj);
       myReader.nextLine();
       while (myReader.hasNextLine()) {
         String data = myReader.nextLine();
-        String splitEach = "[\t]";
-        String[] twoColumn = data.split(splitEach);
-        barcode.add(Integer.valueOf(twoColumn[0]));
-        System.out.println(twoColumn[0]);
+        String regex = "[\t]";
+        String[] twoColumn = data.split(regex);
+        stuID.add(Integer.parseInt(twoColumn[0]));
+        //System.out.println(twoColumn[0]);
+        System.out.println(stuID);
       }
       myReader.close();
 
     } catch (FileNotFoundException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
-    }
+    } /*
     barcode.sort(null);
     boolean numFound;
-    /*Scanner scan = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in);
     System.out.println("Enter a number to search for in the list below");
     int numSearch = scan.nextInt();
-    int len = barcode.size(); */
-    int 
+    int len = barcode.size(); 
+    int constant = barcode.size();
+    
     
     int high=0;
     int low=0;
-    while (true){
+    for (int i = 0;i<constant;i++){
+		while (true){
 
 		int mid = barcode.get(len/2);
 		if (barcode.get(mid)==numSearch){
@@ -70,5 +75,7 @@ public class doubleCrosswalk {
 			}
 		}
 	} 	
-}
+	} */
+}    
+	
 }
